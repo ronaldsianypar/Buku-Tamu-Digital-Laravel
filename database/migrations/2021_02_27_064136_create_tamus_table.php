b63495f4421a72e577d72d8e457a3fe9afbb8b4f
Ronald Sianipar <55264627+ronaldsianypar@users.noreply.github.com>
2021-06-26 22:05:57 +0700

    Update 2021_02_27_064136_create_tamus_table.php

--- database/migrations/2021_02_27_064136_create_tamus_table.php
+++ database/migrations/2021_02_27_064136_create_tamus_table.php
@@ -19,6 +19,7 @@ class CreateTamusTable extends Migration
             $table->string('no_telp', 15);
             $table->text('alamat');
             $table->string('sb_kritik_saran', 100)->nullable();
+            $table->string('sb_saran', 100)->nullable();
             $table->string('keperluan', 100);
             $table->string('sb_instansi', 100)->nullable();
             $table->string('sb_jabatan', 100)->nullable();
